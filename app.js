6116b0b1a62aab7b997271b2ce898cd47cfd55ed
vladislavDev58 <vladislav.dev58@gmail.com>
2021-08-19 15:20:19 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/poisons', require('./routes/poisons.routes'))
 app.use('/api/corn', require('./routes/corn.routes'))
 
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const PORT = config.get('port') || 5000
 const MONGOURL = config.get('mongoUrl')
 
