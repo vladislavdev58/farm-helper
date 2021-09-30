87a433ac3173b7eb6d5305926415203b0b8e18a2
vladislavDev58 <vladislav.dev58@gmail.com>
2021-09-30 20:47:43 +0300

    install notistack

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,12 +3,26 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import {SnackbarProvider} from 'notistack'
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
-  document.getElementById('root')
-);
+    // <React.StrictMode>
+    <SnackbarProvider
+        anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+        }}
+        iconVariant={{
+            success: '✅',
+            error: '✖️',
+            warning: '⚠️',
+            info: 'ℹ️',
+        }}
+    >
+        <App/>
+    </SnackbarProvider>,
+    // </React.StrictMode>,
+    document.getElementById('root')
+)
 
-reportWebVitals();
+reportWebVitals()
